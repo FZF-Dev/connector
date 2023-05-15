f6f55b76993c5a8b5a11595aad7ceed6f0be1d81
FZF-Dev <fzoltan123456@gmail.com>
2023-05-15 20:00:40 +0200

    add base of backend application

---
+++ backend/connector-backend/src/test/java/com/github/fzfdev/connectorbackend/ConnectorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fzfdev.connectorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
